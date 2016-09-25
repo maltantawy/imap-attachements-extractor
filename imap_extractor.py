9f611692dd45d5c249199408c96352299a78c3ce
Mostafa Altantawy <mostafa.altantawy@gmail.com>
2016-09-25 14:01:04 +0200

    url extraction update
    version 0.2

--- imap_extractor.py
+++ imap_extractor.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python -tt
+#!/usr/bin/python -tt
 """
 Author: Mostafa Altantawy 
 Email: Mostafa.Altantawy@Gmail.com
-Version 0.1
+version 0.2
 """
-import imaplib, email 
+import imaplib, email, re
 from ConfigParser import ConfigParser
 
 
@@ -14,10 +14,11 @@ config.read('config.ini')
 mail_server = config.get('server_config', 'server_hostname')
 user_name = config.get('server_config', 'user_name')
 password = config.get('server_config', 'password')
-path = config.get('file_path', 'path')
+files_path = config.get('path', 'files_path')
+urls_path = config.get('path', 'urls_path') 
 
 
-class GetMail():
+class GetEmail():
 	connection = None
 	error = None
 
@@ -54,9 +55,29 @@ class GetMail():
 		for emailid in items:
 			self.download_attachments_in_email(self.connection, emailid, outputdir)
 
+	def extract_url(self):
+		urls_dict = dict()
+		response, items = self.connection.search(None, "(ALL)")
+		items = items[0].split()
+		for emailid in items:
+			response, data = self.connection.fetch(emailid, "(BODY.PEEK[])")
+			email_body = data[0][1]
+			mail = email.message_from_string(email_body)
+			for part in mail.walk():
+				if re.findall(r'(https?://\S+)', str(part)):
+					urls_dict[part['Message-ID']] = re.findall(r'(https?://\S+)', str(part))
+		for k, v in urls_dict.items():
+			urls_dict[k] = ''.join(v)
+		
+		urls =  open(str(urls_path) + '/' + 'urls.txt', 'w')
+		for k, v in sorted(urls_dict.items()):
+			urls.write(str(k) +','+str(v) + '\n')
+		urls.close()
+
 
 		
 
-imap_connect = GetMail(mail_server, user_name, password)
-imap_connect.download_all_attachments_in_inbox(mail_server, user_name, password, path)
+imap_connect = GetEmail(mail_server, user_name, password)
+imap_connect.extract_url()
+imap_connect.download_all_attachments_in_inbox(mail_server, user_name, password, files_path)
 imap_connect.close_connection()
